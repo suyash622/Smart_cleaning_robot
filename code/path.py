c34cbfdaba9f69013530adc482bff41030d314b3
suyash622 <suyash622@gmail.com>
2018-04-16 22:13:07 -0400

    Boiler Plate for path planning

--- code/path.py
+++ code/path.py
@@ -1,14 +1,35 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import netwrokX
+import networkx as nx
+
 
 class gen_path(object):
-	def __init__(node_data,edge_data,special_nodes):
+	def __init__(node_data,edge_data,special_edges):
 		self.node_data=node_data
 		self.edge_data=edge_data
-		self.special_nodes=special_nodes
+		self.special_nodes=special_edges
 		self.path=[]
-		self.G=[]
+		self.priority = []
+		self.G=nx.Graph()
 
 
-	def 
+	def build_graph():
+		for i in node_data:
+			self.G.add_node(i)
+		for e in edge_data:
+			n1 = e[0]
+			n2 = e[1]
+			weight = e[3]
+			self.G.add_edge(n1,n2)
+			self.G[n1][n2]['weight'] = weight
+
+	def Heurestics():
+		for i in special_edges:
+			self.priority.append(i)
+
+	def master_path():
+
+	def display_path():
+
+	]
+
